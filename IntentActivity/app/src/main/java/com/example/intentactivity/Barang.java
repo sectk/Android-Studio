edfb6737cc02e7d189ba49cdcc5d027ef7e22bfc
EnditaDwi <enditadwi3@gmail.com>
2024-08-13 21:17:37 +0700

    IntentActivity_Barang

---
+++ IntentActivity/app/src/main/java/com/example/intentactivity/Barang.java
@@ -0,0 +1,36 @@
+package com.example.intentactivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Barang extends AppCompatActivity {
+
+    TextView tvbarang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_barang);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        load();
+        ambilData();
+
+    }
+    public  void load(){
+        tvbarang = findViewById(R.id.tvBarang);
+    }
+    public void ambilData(){
+        String ambil = getIntent().getStringExtra("ISI");
+        tvbarang.setText(ambil);
+    }
+}
