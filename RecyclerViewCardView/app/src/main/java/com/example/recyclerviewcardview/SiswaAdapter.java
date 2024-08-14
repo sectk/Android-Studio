f4c6f439cfba0dd452b9f06a19e4dd061babe38b
EnditaDwi <enditadwi3@gmail.com>
2024-08-14 12:43:41 +0700

    Recycler

--- RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/SiswaAdapter.java
+++ RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/SiswaAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -33,6 +34,13 @@ public class SiswaAdapter extends  RecyclerView.Adapter<SiswaAdapter.ViewHolder>
         Siswa siswa = siswaList.get(i);
         viewHolder.tvNama.setText(siswa.getNama());
         viewHolder.tvAlamat.setText(siswa.getAlamat());
+
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Nama : "+siswa.getNama()+" Alamat : "+siswa.getAlamat(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
