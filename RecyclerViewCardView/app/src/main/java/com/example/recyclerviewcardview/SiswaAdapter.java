04500fb0d7332f8ecd436175c65d9f3d481caeb0
EnditaDwi <enditadwi3@gmail.com>
2024-08-14 11:56:55 +0700

    Recycler

---
+++ RecyclerViewCardView/app/src/main/java/com/example/recyclerviewcardview/SiswaAdapter.java
@@ -0,0 +1,55 @@
+package com.example.recyclerviewcardview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class SiswaAdapter extends  RecyclerView.Adapter<SiswaAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Siswa> siswaList;
+
+    public SiswaAdapter(Context context, List<Siswa> siswaList) {
+        this.context = context;
+        this.siswaList = siswaList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_siswa, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Siswa siswa = siswaList.get(i);
+        viewHolder.tvNama.setText(siswa.getNama());
+        viewHolder.tvAlamat.setText(siswa.getAlamat());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return siswaList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvNama, tvAlamat;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvNama = itemView.findViewById(R.id.tvNama);
+            tvAlamat = itemView.findViewById(R.id.tvAlamat);
+        }
+    }
+}
