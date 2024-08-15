3af5d1ea4c447cbc7b7cad843adad9588e5b9cd5
EnditaDwi <enditadwi3@gmail.com>
2024-08-15 08:07:35 +0700

    Sqlite_Folder

--- SQLiteDatabase/app/src/main/java/com/example/sqlitedatabase/MainActivity.java
+++ SQLiteDatabase/app/src/main/java/com/example/sqlitedatabase/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sqlitedatabase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Database db;
+    EditText etBarang, etStok, etHarga;
+    TextView tvPilihan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +31,38 @@ public class MainActivity extends AppCompatActivity {
     }
     public void  load(){
     db = new Database(this);
-
     db.buatTabel();
+
+    etBarang = findViewById(R.id.etBarang);
+    etStok  = findViewById(R.id.etStok);
+    etHarga = findViewById(R.id.etHarga);
+    tvPilihan = findViewById(R.id.tvPilihan);
+    }
+
+    public  void simpan(View v){
+        String barang = etBarang.getText().toString();
+        String stok = etStok.getText().toString();
+        String harga = etHarga.getText().toString();
+        String pilihan = tvPilihan.getText().toString();
+
+        if(barang.isEmpty() || stok.isEmpty() || harga.isEmpty()){
+            pesan("Data Kosong");
+        }else {
+            if (pilihan.equals("insert")){
+                String sql = "INSERT INTO tblbarang (barang,stok,harga) VALUES ('"+barang+"',"+stok+","+harga+")";
+                db.runSQL(sql);
+                pesan("insert berhasil");
+            }else {
+                pesan("update");
+            }
+        }
+
+        etBarang.setText("");
+        etStok.setText("");
+        etHarga.setText("");
+        tvPilihan.setText("insert");
+    }
+    public void pesan (String isi){
+        Toast.makeText(this, isi, Toast.LENGTH_SHORT).show();
     }
 }
